4634afd6d23ad412f1fa95726e1cf12bb146046c
marceta <dalibor.marceta@gmail.com>
2017-06-07 22:03:35 -0700

    Test

--- src/Day2_BalancedTree.java
+++ src/Day2_BalancedTree.java
@@ -3,41 +3,97 @@
  * Created by marceta on 6/7/17.
  */
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+public class Binarytree
+{
+    private static BTNode root;
 
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-public class Day2_BalancedTree {
-
-    public static void main(TreeNode root) {
-        // write your code here
-
-        boolean result = isBalanced(root);
-        System.out.println( "Node is balanced " + result);
-
+    public Binarytree(int data)
+    {
+        root = new BTNode(data);
     }
 
-    public static boolean isBalanced(TreeNode root) {
+    public void add(BTNode parent, BTNode child, String orientation)
+    {
+        if (orientation.equals("left"))
+        {
+            parent.setLeft(child);
+        }
+        else
+        {
+            parent.setRight(child);
+        }
+    }
+
+    public static boolean isBalanced(BTNode root) {
+
         return (maxdepth(root) - mindepth(root) < 2);
     }
 
-    private static int maxdepth(TreeNode root) {
+    public static int maxdepth(BTNode root) {
         if (root == null) {
             return 0;
         }
         return 1 + Math.max(maxdepth(root.left), maxdepth(root.right));
     }
 
-    private static int mindepth(TreeNode root) {
+    public static int mindepth(BTNode root) {
         if (root == null) {
             return 0;
         }
         return 1 + Math.min(mindepth(root.left), mindepth(root.right));
     }
+
+    public static void main(String args[])
+    {
+        BTNode n1 = new BTNode(1);
+        BTNode n2 = new BTNode(4);
+        BTNode n3 = new BTNode(2);
+        BTNode n4 = new BTNode(5);
+
+        Binarytree tree = new Binarytree(3); //  3
+        tree.add(root, n1, "left"); //         1/ \
+        tree.add(root, n2, "right"); //            4
+        tree.add(n2, n3, "left"); //             2/ \
+        tree.add(n2, n4, "right"); //                5
+
+        boolean result = isBalanced(root);
+        System.out.println( "Node is balanced " + result);
+    }
+}
+
+class BTNode {
+    private int key;
+    private BTNode left;
+    private BTNode right;
+
+    BTNode (int key) {
+        this.key = key;
+        right = null;
+        left = null;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setLeft(BTNode left) {
+        this.left = left;
+    }
+
+    public BTNode getLeft() {
+        return left;
+    }
+
+    public void setRight(BTNode right ) {
+        this.right = right;
+    }
+
+    public BTNode getRight() {
+        return right;
+    }
+
 }
